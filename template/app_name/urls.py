1b131c7969a34d6053a223bf5a582de328ce22fc
Jon Bolt <epic.bagel@gmail.com>
2013-11-06 14:24:50 +0000

    Update

---
+++ template/app_name/urls.py
@@ -0,0 +1,6 @@
+from django.conf.urls import patterns, include, url
+from views import *
+
+urlpatterns = patterns('',
+	#url(r'^foo/(?P<pk>\d+)/', DeleteChargeView.as_view(), {}, name = 'delete-charge'),
+)
